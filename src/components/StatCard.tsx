78a07780e6a78906a3d4d3b9109eeceeddc71b35
Henry Weng <henryweng03@gmail.com>
2024-06-22 16:54:14 -0700

    style: small changes to card styling

--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -8,8 +8,10 @@ type StatCardProps = {
 export default function StatCard({ header, text }: StatCardProps) {
   return (
     <Card className="p-8 shadow-sm max-w-[18rem]">
-      <h1 className="text-4xl font-medium mb-2">{header}</h1>
-      <p className="text-slate-700 leading-5">{text}</p>
+      <h1 className="text-4xl text-slate-950 font-medium mb-2 tracking-tight">
+        {header}
+      </h1>
+      <p className="text-slate-600 leading-5">{text}</p>
     </Card>
   );
 }
